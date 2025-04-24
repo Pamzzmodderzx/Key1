04b5977be5a2560a659b67aa28970276c5b75f26
Pamzzmodderzx <95611178+Pamzzmodderzx@users.noreply.github.com>
2025-04-25 03:15:39 +0700

    Create Key.js

---
+++ Key.js
@@ -0,0 +1,17 @@
+{
+
+  "secretCode": "xyzdzzv1",
+
+  "description": "PamzzNotDev", 
+
+  "lastUpdated": "25/04/2025",
+
+  "author": "PamzzGanteng++", 
+
+  "status": "Active",
+
+  "license": "MIT",
+
+  "version": "1.0"
+
+}
